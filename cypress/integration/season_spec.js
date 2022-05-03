30f307e48a298cfdce0b34fe2a4013c607904871
David Schultz <davids24@gmail.com>
2022-05-02 23:27:02 -0500

    fix season test

--- cypress/integration/season_spec.js
+++ cypress/integration/season_spec.js
@@ -28,14 +28,16 @@ describe('seasons', () => {
     cy.get('[data-test=season]').within(() => {
       cy.get('[data-test=player-name]:nth-child(2)').invoke('text').invoke('trim').should('equal', 'Player4')
       cy.get('[data-test=player-name]:nth-child(3)').invoke('text').invoke('trim').should('equal', 'Player1')
-      cy.get('[data-test=player-name]:nth-child(4)').invoke('text').invoke('trim').should('equal', 'Player2')
-      cy.get('[data-test=player-name]:nth-child(5)').invoke('text').invoke('trim').should('equal', 'Player3')
+      cy.get('[data-test=player-name]:nth-child(4)').invoke('text').invoke('trim').should('equal', 'Player6')
+      cy.get('[data-test=player-name]:nth-child(5)').invoke('text').invoke('trim').should('equal', 'Player5')
+      cy.get('[data-test=player-name]:nth-child(6)').invoke('text').invoke('trim').should('equal', 'Player2')
+      cy.get('[data-test=player-name]:nth-child(7)').invoke('text').invoke('trim').should('equal', 'Player3')
 
       cy.get('[data-test=2021-01-01]').should('exist').within(() => {
         cy.get('.cell:nth-child(2)').should('contain', '7.5 + 4')
         cy.get('.cell:nth-child(3)').should('contain', '6.5 + 4')
-        cy.get('.cell:nth-child(4)').should('contain', '2.5 + 0')
-        cy.get('.cell:nth-child(5)').should('contain', '1.5 + 0')
+        cy.get('.cell:nth-child(6)').should('contain', '2.5 + 0')
+        cy.get('.cell:nth-child(7)').should('contain', '1.5 + 0')
       })
     })
   })
